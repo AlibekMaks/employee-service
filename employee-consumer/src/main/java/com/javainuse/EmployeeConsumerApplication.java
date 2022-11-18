80a1d25487f65c7450a98c3963e09bad34dcc32e
Alibek <a.maksut@platonus.kz>
2022-11-18 22:20:54 +0600

    some changes

--- employee-consumer/src/main/java/com/javainuse/EmployeeConsumerApplication.java
+++ employee-consumer/src/main/java/com/javainuse/EmployeeConsumerApplication.java
@@ -6,17 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClientException;
-
-import com.javainuse.controllers.ConsumerControllerClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringBootHelloWorldApplication {
+public class EmployeeConsumerApplication {
 
     public static void main(String[] args) throws RestClientException, IOException {
-        ApplicationContext ctx = SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(EmployeeConsumerApplication.class, args);
 
 /*
         ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
